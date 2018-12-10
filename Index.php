60206eeb5247314b5d74dbec8a577c2c42b7cdbe
AshifRaihun002 <43530945+AshifRaihun002@users.noreply.github.com>
2018-12-10 21:00:15 +0600

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,75 @@
+<?php ?>
+<html>
+<head>
+    <title>Home</title>
+	<link href='style.css' rel='stylesheet'>
+</head>
+
+<body>
+    <header>
+	   <div class="row">
+		<ul class="main-nav1">
+		    <li class="active"><a href="#">Students</a></li>
+			<li><a href="#">Staff</a></li>
+			<li><a href="#">Alumni</a></li>
+			<li><a href="#">myHub</a></li>
+		</ul>
+	    </div>
+		
+		<div class="neon1">
+		  <input name="" type="text">
+            <input value="Search" type="button">
+	    </div>	
+	 </header>
+	 
+	<div class="neon">
+		    <h3 data="neon">University of Manchester</h3>
+	    </div>
+		
+		<div class="row">
+		<ul class="main-nav">
+		    <li class="active"><a href="#">Home</a></li>
+			<li><a href="#">Why MU?</a></li>
+			<li><a href="#">Study</a></li>
+			<li><a href="#">Research</a></li>
+			<li><a href="#">Collaborate</a></li>
+			<li><a href="#">Global BU</a></li>
+			<li><a href="#">About</a></li>
+		</ul>
+		</div>
+		
+		<div class="div3">
+		    <a class="test" href="#">Why MU?</a>
+			<a class="test" href="#">Thems</a>
+			<a class="test" href="#">Centres</a>
+			<a class="test" href="#">Impact</a>
+			<a class="test" href="#">Environment</a>
+			<a class="test" href="#">Postgraduate</a>
+			<a class="test" href="#">Our latest research</a>
+			<a class="test" href="#">Research Chronicle</a>
+			<a class="test" href="#">undergraduate</a>
+			<br/>
+			<a class="test" href="#">British Conference of Undergraduate Research</a>
+			<a class="test" href="#">Showcasing Undergraduate Research Excellence</a>
+		</div>
+		
+		<div class="image">
+		   
+		</div>
+		
+		<div class="text">
+		    <h4 data="text">Our researh shapes and changes the world around us providing solutions to real world problems and informing the education we deliver.</h4>
+	    </div>
+		
+		<div class="image1">
+		      <img src="2.jpg" alt="pic1">
+			  <img src="3.png" alt="pic2">
+		</div>
+		
+		<div class="image2">
+		   
+		</div>
+		
+	           
+</body>
+</html>
